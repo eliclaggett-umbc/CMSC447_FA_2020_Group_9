362abaec7dca9a4904090b23bafe80de6a6b1ada
Kayley <admin@admins-MBP-2.fios-router.home>
2020-10-30 22:29:07 -0400

    add search api endpoints, and sum/avg aggregation

--- app/pages/api/counties.js
+++ app/pages/api/counties.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
     // save route and query params.
     const { 
-        query: {date, search}
+        query: {date, sum, avg}
     } = req
 
    var today = new Date();
@@ -25,52 +25,60 @@ export default async function handler(req, res) {
     var query_params = {
         // default to today's date
         date: date ? date : `${today.getFullYear() + '-' + ( today.getMonth() + 1 ) + '-' + today.getDate()}`,
-        search: search ? search : null
+        sum: sum &&  sum.toLowerCase() == 'true' ? true : false,
+        avg: avg && avg.toLowerCase() == 'true' ? true : false
     }
 
-    
     var query_string = null
-    var error_message = null
 
-    // default status code
-    res.statusCode = 200
+    if(query_params.date) {
+    // verify date string is in correct format
+        const regex_match = query_params.date.match(/[0-9]{4}-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])/)
+        // incorrect date
+        if(!regex_match) {
+            res.statusCode = 400
+            res.status(400).end("bad request")
+        }
+    }
 
 
     if(req.method == "GET") {
 
-        // query based on date
-        if (!query_params.search) {
-
-            // verify date string is in correct format
-            const regex_match = query_params.date.match(/[0-9]{4}-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])/)
-            // incorrect date
-            if(!regex_match) {
-                res.statusCode = 400
-                error_message = res.statusCode + " bad request"
-            }
-
-            // query all county covid data on date
+        // query all county covid data on date
+        if(!query_params.sum && !query_params.avg) {
+            
+            
+            
             query_string = `
             SELECT county_covid.*, population FROM county_covid
             INNER JOIN (select county, state, max(date) AS date FROM county_covid
-                WHERE date<='${query_params.date}' GROUP BY county, state) recent_covid_data
-                ON recent_covid_data.county = county_covid.county AND recent_covid_data.state = county_covid.state
-                AND recent_covid_data.date = county_covid.date
+            WHERE date<='${query_params.date}' GROUP BY county, state) recent_covid_data
+            ON recent_covid_data.county = county_covid.county AND recent_covid_data.state = county_covid.state
+            AND recent_covid_data.date = county_covid.date
             INNER JOIN county ON county_covid.county = county.county AND county_covid.state = county.state`
-
-            // query county covid data for each county, closest to date
             
         }
-
-        // query state and county from search results (auto completion)
+            // query aggregate county data
         else {
-            query_string = `SELECT county, state FROM county WHERE county LIKE '%' || '${query_params.search}' || '%'`
-        }
 
-        // query parameters do not match required format
-        if(res.statusCode != 200) res.end(error_message)
+                var query_aggregate = ''
+                const query_start = `SELECT max(date) AS date`
+                const query_end = `FROM county_covid WHERE date<='${query_params.date}'`
 
-        // query county data
+                // add sum aggregates
+                if(query_params.sum) {
+                    query_aggregate = `SUM(Cases) AS "total cases", SUM(deaths) AS "total deaths"`
+                }
+                // add average aggregates 
+                if(query_params.avg) {
+                    query_aggregate += query_aggregate ? `, avg(Cases) AS "average cases", avg(deaths) AS "average deaths"` : `avg(Cases) AS "average cases", avg(deaths) AS "average deaths"`
+                }
+
+                // form full query string
+                query_string = query_start + ',\n' + query_aggregate + '\n' + query_end
+            }
+
+        // perform query
         try {
             const result = await pool.query(query_string)
             res.send(result.rows)
--- app/pages/api/prisons.js
+++ app/pages/api/prisons.js
@@ -12,15 +12,18 @@ export default async function handler(req, res) {
 
     // save route and query params.
     const { 
-        query: {date, search}
+        query: {date, sum, avg}
     } = req
+
+    
 
     var today = new Date();
 
     var query_params = {
         // default to today's date
         date: date ? date : `${today.getFullYear() + '-' + ( today.getMonth() + 1 ) + '-' + today.getDate()}`,
-        search: search ? search : null
+        sum: sum &&  sum.toLowerCase() == 'true' ? true : false,
+        avg: avg && avg.toLowerCase() == 'true' ? true : false
     }
 
     
@@ -31,45 +34,53 @@ export default async function handler(req, res) {
     res.statusCode = 200
 
 
+
+    if(query_params.date) {
+    // verify date string is in correct format
+        const regex_match = query_params.date.match(/[0-9]{4}-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])/)
+        // incorrect date
+        if(!regex_match) {
+            res.statusCode = 400
+            res.status(400).end("bad request")
+        }
+    }
+
+        
     if(req.method == "GET") {
+            
 
-        // query based on given date
-        if (!query_params.search) {
-
-
-
-            // verify date string is in correct format
-            const regex_match = query_params.date.match(/[0-9]{4}-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])/)
-            // incorrect date
-            if(!regex_match) {
-                res.statusCode = 400
-                error_message = res.statusCode + " bad request"
-            }
-
-            // query prison covid data for each prison, closest to date
-
-            query_string = `
-            SELECT state, prison_covid.* FROM prison_covid
-            INNER JOIN (select prison, county, max(date) AS date FROM prison_covid
-            WHERE date<='${query_params.date}' GROUP BY prison, county) recent_covid_data
-                    ON recent_covid_data.county = prison_covid.county AND recent_covid_data.prison = prison_covid.prison
-                    AND recent_covid_data.date = prison_covid.date
-            INNER JOIN prison ON prison.prison = prison_covid.prison AND prison.county = prison_covid.county
-            `
-        }
-
-        // query state and county and prison from search results (auto completion)
-        else {
-
-            query_string = `
-            SELECT prison, county, state FROM prison WHERE prison LIKE '%' || '${query_params.search}' || '%'
-            `
-            }
-        }
+        
+        // query prison covid data for each prison, closest to date
+            if(!query_params.sum && !query_params.avg) {
 
 
-        if(res.statusCode != 200) res.end(error_message)
-
+                query_string = `
+                SELECT state, prison_covid.* FROM prison_covid
+                INNER JOIN (select prison, county, max(date) AS date FROM prison_covid
+                WHERE date<='${query_params.date}' GROUP BY prison, county) recent_covid_data
+                        ON recent_covid_data.county = prison_covid.county AND recent_covid_data.prison = prison_covid.prison
+                        AND recent_covid_data.date = prison_covid.date
+                INNER JOIN prison ON prison.prison = prison_covid.prison AND prison.county = prison_covid.county
+                `
+            }     // query aggregate county data
+            else {
+    
+                    var query_aggregate = ''
+                    const query_start = `SELECT max(date) AS date`
+                    const query_end = `FROM prison_covid WHERE date<='${query_params.date}'`
+    
+                    // add sum aggregates
+                    if(query_params.sum) {
+                        query_aggregate = `SUM(pop_tested_positive) AS "total cases", SUM(pop_deaths) AS "total deaths"`
+                    }
+                    // add average aggregates 
+                    if(query_params.avg) {
+                        query_aggregate += query_aggregate ? `, avg(pop_tested_positive) AS "average cases", avg(pop_deaths) AS "average deaths"` : `avg(pop_tested_positive) AS "average cases", avg(pop_deaths) AS "average deaths"`
+                    }
+    
+                    // form full query string
+                    query_string = query_start + ',\n' + query_aggregate + '\n' + query_end
+                }
 
         try {
             const result = await pool.query(query_string)
@@ -79,3 +90,4 @@ export default async function handler(req, res) {
         }
 
   }
+}
